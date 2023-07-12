fc2a4aa56dca81236555ca09dcc36c03f0334961
Abhinab Dutta Gupta <94664667+BLITZKRE1G@users.noreply.github.com>
2023-07-12 12:32:37 +0530

    Update MailController.java
    added Controller method to send BasicEmail

--- email/src/main/java/personal/project/email/controller/MailController.java
+++ email/src/main/java/personal/project/email/controller/MailController.java
@@ -2,7 +2,9 @@ package personal.project.email.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +22,7 @@ public class MailController {
 
     @ResponseBody
     @PostMapping(value = "/approval-mail")
-    public String sendApprovalMail(ApprovalRequest approvalRequest) {
+    public String sendApprovalMail(@RequestBody ApprovalRequest approvalRequest) {
         try {
             log.info("Sending Approval Mail to: {}", approvalRequest.getMailId());
             service.sendApprovalMail(approvalRequest);
@@ -29,5 +31,18 @@ public class MailController {
             return "ABORTED";
         }
         return "Mail Sent!\nPort Terminated for further use";
+    }
+
+    @ResponseBody
+    @PostMapping(value = "/basic-mail")
+    public String sendBasicMail(@RequestParam String toEmail, @RequestParam String subject, @RequestParam String emailText) {
+        try {
+            log.info("Sending Mail without Templates");
+            service.sendBasicEmail(toEmail, subject, emailText);
+        } catch (Exception exception) {
+            log.info("Exception Encountered while sending");
+            return "Exception Encountered while sending\nABORTED";
+        }
+        return "Mail Sent!\nPort Terminated for Further use!";
     }
 }
